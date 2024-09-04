f4f3a028963da73df425584431f368cdb10dbd0f
KarolineMK <kakj0003@stud.kea.dk>
2024-09-04 16:01:51 +0200

    Er færdig med at gøre hjemmesiden dynamisk

--- js/category.js
+++ js/category.js
@@ -1,22 +1,16 @@
-const categoryList = document.querySelector("#categorylist");
-const params = URLSearchParams(document.location.search);
-const category = params.get("category");
-let url = undefined; 
+const categoryList = document.querySelector("#categoryList");
+
 
 // kalder kategorierne/ laver en request 
 // gemmer det i en varibel i json format, som gives videre til brugeren 
-fetch("https://kea-alt-del.dk/t7/api/categories").then(response=>response.json()).then(categories =>{
+fetch("https://kea-alt-del.dk/t7/api/categories")
+.then(response=>response.json())
+.then(categories =>{
     categories.forEach((category)=>{
-        categoryList.innerHTML += `<li><a href="index.html?category=${category.category}">${category.category}</a></liv>`;
+        categoryList.innerHTML += `<li><a href="productlist.html?category=${category.category}">${category.category}</a></liv>`;
     }); 
     // categoryList.innerHTML = `<li>${categories.category}</li>`
 });
 
 // params = alle url parameter 
-if(params.has("category")){
-    url = "https://kea-alt-del.dk/t7/api/prdoucts?category=" + category;
-}else{
-    url = "https://kea-alt-del.dk/t7/api/prdoucts";
-}
-    
 
--- js/productlist.js
+++ js/productlist.js
@@ -1,12 +1,16 @@
 window.addEventListener("DOMContentLoaded", init);
 
-const url = `https://kea-alt-del.dk/t7/api/products?limit=15`;
-const skabelon = document.querySelector("template").content;
-const container = document.querySelector("main");
+const params = new URLSearchParams(document.location.search);
+const category = params.get("category");
 
-fetch ("https://kea-alt-del.dk/t7/api/products")
-.then(res=>res.json())
-.then(showProducts)
+const url = `https://kea-alt-del.dk/t7/api/products?category=` + category;
+const skabelon = document.querySelector("template").content;
+const container = document.querySelector(".productlistMain");
+
+
+
+
+
 
 function init(){
     fetch(url)
@@ -15,19 +19,31 @@ function init(){
 }
 
 function showProducts(products){
+    console.log(products)
     products.forEach((product)=> {
         const kopi = skabelon.cloneNode(true);
         kopi.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
         kopi.querySelector("img").alt = product.productdisplayname;
         kopi.querySelector("h3").textContent = product.productdisplayname;
         kopi.querySelector(".price").textContent = product.price;
-        kopi.querySelector("a").href = `product.html?id=${product.id}`;
         kopi.querySelector(".subtle").textContent = product.brandname;
+        kopi.querySelector("a").href = `product.html?id=${product.id}`;
+        
+        if (product.soldout) {
+            kopi.querySelector(".smallProduct").classList.add("soldOut");
+          }
+        if (product.discount) {
+            kopi.querySelector(".smallProduct").classList.add("onSale");
+            kopi.querySelector(".discounted p").textContent = "Now DKK " + Math.round(product.price - (product.price * product.discount) / 100);
+            kopi.querySelector(".discountProcent ").textContent ="-" + product.discount + "%";
+        } else{
+            kopi.querySelector(".discounted").style.display ="none";
+        }
+
         container.appendChild(kopi);
     })
 
-    kopi.querySelector(".read_more").setAttribute("href", `product.html?id=${product.id}`);
     //append 
-    document.querySelector(".product_list").appendChild(copy);
+    // document.querySelector(".product_list").appendChild(kopi);
 }
 
